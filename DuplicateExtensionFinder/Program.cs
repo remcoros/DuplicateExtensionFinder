4d4d398c5f2ecea2bd8385637c80ab114d4cad01
tom-englert <mail@tom-englert.de>
2016-12-28 11:17:13 +0100

    Handle UnauthorizedAccessException

--- DuplicateExtensionFinder/Program.cs
+++ DuplicateExtensionFinder/Program.cs
@@ -52,7 +52,7 @@
                             {
                                 if (vsixSerializer.CanDeserialize(rdr))
                                 {
-                                    var vsix = (Vsix) vsixSerializer.Deserialize(rdr);
+                                    var vsix = (Vsix)vsixSerializer.Deserialize(rdr);
                                     extensions.Add(new Extension()
                                     {
                                         Id = vsix.Identifier.Id,
@@ -64,7 +64,7 @@
                                 }
                                 else if (packageSerializer.CanDeserialize(rdr))
                                 {
-                                    var package = (PackageManifest) packageSerializer.Deserialize(rdr);
+                                    var package = (PackageManifest)packageSerializer.Deserialize(rdr);
                                     extensions.Add(new Extension()
                                     {
                                         Id = package.Metadata.Identity.Id,
@@ -97,7 +97,18 @@
 
                     if (isDuplicate && doDelete)
                     {
-                        Directory.Delete(vsix.Path, true);
+                        try
+                        {
+                            Directory.Delete(vsix.Path, true);
+                        }
+                        catch (System.UnauthorizedAccessException)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine("You must start as administrator to delete global extensions.");
+                            Console.WriteLine("Press any key to continue...");
+                            Console.ReadKey();
+                            return;
+                        }
                     }
                 }
 
